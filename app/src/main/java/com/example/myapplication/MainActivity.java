4c77a1a8ae8d022309c972f874abf7776e0216bd
user376-programist <banaszkiewicz.piotr@zst.radom.pl>
2025-02-13 12:14:37 +0100

    App mechanic finished

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -16,24 +17,43 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         SeekBar seekBar = findViewById(R.id.seekBar);
         Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = (String) textView.getText();
+                textView.setText(Welcome(text));
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setTextSize((float) i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public String Welcome(String text){
         switch(text){
             case "Dzień dobry":
-                return "Hello";
-            break;
-            case "Hello":
-                return "Guten Tag";
-            break;
-            case "Guten Tag":
+                return "Good morning";
+            case "Good morning":
                 return "Buenos dias";
-            break;
             case "Buenos dias":
                 return "Dzień dobry";
-            break;
             default:
-                return text;
+               return text;
         }
     }
 }
